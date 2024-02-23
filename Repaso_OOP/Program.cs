e17d692a3f3c4ecee191b7c44ae2f950176b670f
sigmotoa <si.galvismotoa@gmail.com>
2024-02-23 16:32:10 -0500

    Hello World

--- Repaso_OOP/Program.cs
+++ Repaso_OOP/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("sigmotoa");
+Console.WriteLine("Hello sigmotoa");
+int edad;
+edad = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine($"Hola, ya sabemos que tienes {edad} años");
