5dd5a2088a82ebe3ea2e5658e1e02d16bf13b73f
sigmotoa <si.galvismotoa@gmail.com>
2024-02-23 16:47:27 -0500

    Usando aritmeticos

--- Repaso_OOP/Program.cs
+++ Repaso_OOP/Program.cs
@@ -1,6 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello sigmotoa");
+/*Console.WriteLine("Hello sigmotoa");
 int edad;
 edad = Convert.ToInt16(Console.ReadLine());
 Console.WriteLine($"Hola, ya sabemos que tienes {edad} años");
+double dinero;
+dinero = 320.5d;
+string name = "sigmotoa";
+Console.WriteLine("Hola "+name+" Bienvenido con tus "+dinero+" pesos");
+
+var algo = 0.1f;
+
+Console.WriteLine(dinero*algo);*/
+
+
+//Operadores aritmeticos
+// + - * / %
+
+double numA;
+numA = 0.5;
+double numB;
+numB = 7.1;
+
+Console.WriteLine($"La suma es {numA+numB}\nLa resta es{numA-numB}" +
+                  $"\nEl producto es {numA*numB}"+
+                  $"\nEl cociente es {numA/numB}");
+
+
+
+
+
+
+
+
+
+
+
